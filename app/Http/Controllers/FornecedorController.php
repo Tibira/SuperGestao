94ed1dbcc532de10f9aa0ddbb07c3b7803f82257
Tibira <tibirasantos3b@gmail.com>
2022-04-15 15:22:04 -0300

    Seção 7 - Aula 45

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    //
+    public function index(){
+        $fornecedores = ['Fornecedor 01'];
+        return view('app.fornecedores.index', compact('fornecedores'));
+    }
 }
---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,9 @@
+<h3>Fornecedores</h3>
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Não existe fornecedores cadastrados</h3>
+@endif
