04f0a6932ae431dfc919d36d7c359abef9e82514
Tibira <tibirasantos3b@gmail.com>
2022-04-20 17:23:07 -0300

    Seção 7 - Aula 61

--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/','PrincipalController@principal')->name('site.index');
-Route::get('/sobre-nos','SobreNosController@sobreNos')->name('site.sobre-nos');
-Route::get('/contato','ContatoController@contato')->name('site.contato');
-Route::get('/login','PrincipalController@principal')->name('site.login');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
 
-
-Route::prefix('/app')->group(function(){
-    Route::get('/clientes','ClientesController@index')->name('app.cientes');
-    Route::get('/fornecedores','FornecedoresController@index')->name('app.fornecedores');
-    Route::get('/produtos','ProdutosController@index')->name('app.produtos');
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
 });
 
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
 
-Route::get('/teste/{p1}/{p2}' ,'TesteController@teste')->name('site.teste');
-
-Route::fallback(function(){
-    echo 'fallback';
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
 });
