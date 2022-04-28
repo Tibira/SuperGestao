2ea5283f5569de9060e0a0d9b75b435c95609c1f
Tibira <tibirasantos3b@gmail.com>
2022-04-28 19:09:33 -0300

    Seção 8 - Aula 84

---
+++ app/Fornecedor.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fornecedor extends Model
+{
+    use HasFactory;
+}
